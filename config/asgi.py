efab8c3541fb59d7c71c00960dba050042a8a122
Jamugisa-Peter-Paul <jamugisapeter21@gmail.com>
2025-11-04 19:02:36 +0300

    remove WSGI WhiteNoise wrapper

--- config/asgi.py
+++ config/asgi.py
@@ -2,7 +2,6 @@ import os
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
-from whitenoise import WhiteNoise
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
@@ -11,7 +10,7 @@ django_asgi_app = get_asgi_application()
 import chat.routing
 
 application = ProtocolTypeRouter({
-    "http": WhiteNoise(django_asgi_app, root="staticfiles"),
+    "http": django_asgi_app,
     "websocket": AuthMiddlewareStack(
         URLRouter(chat.routing.websocket_urlpatterns)
     ),
